cf0281fae70f740e3d0ab558e6b80cd284ce6ed6
Xhemajl Camaj <Xhemajlc@Agrons-MacBook-Pro-2.local>
2022-01-19 14:18:17 -0500

    create arrays for edit and delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var taskIdCounter = 0
 var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
+var tasks = [];
 
 var completeEditTask = function (taskName, taskType, taskId) {
     // find the matching task list item
@@ -12,6 +13,14 @@ var completeEditTask = function (taskName, taskType, taskId) {
     // set new values
     taskSelected.querySelector("h3.task-name").textContent = taskName;
     taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    // loop through tasks array and task object with new content
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+            tasks[i].name = taskName;
+            tasks[i].type = taskType;
+        }
+    };
 
     alert("Task Updated!");
     formEl.removeAttribute("data-task-id");
@@ -37,11 +46,6 @@ var taskFormHandler = function () {
 
     var isEdit = formEl.hasAttribute("data-task-id");
 
-    // package up data as an object
-    var taskDataObj = {
-        name: taskNameInput,
-        type: taskTypeInput
-    };
 
     // has data attribute, so get task id and call function to complete edit process
     if (isEdit) {
@@ -52,7 +56,8 @@ var taskFormHandler = function () {
     else {
         var taskDataObj = {
             name: taskNameInput,
-            type: taskTypeInput
+            type: taskTypeInput,
+            status: "to do",
         };
 
         createTaskEl(taskDataObj);
@@ -60,6 +65,10 @@ var taskFormHandler = function () {
 };
 
 var createTaskEl = function (taskDataObj) {
+
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
+
     // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -72,6 +81,10 @@ var createTaskEl = function (taskDataObj) {
     taskInfoEl.className = "task-info";
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
+
+    taskDataObj.id = taskIdCounter;
+
+    tasks.push(taskDataObj);
 
     var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);
@@ -148,11 +161,22 @@ var taskButtonHandler = function (event) {
 
 
 var deleteTask = function (taskId) {
-    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
-    taskSelected.remove();
+    // create new array to hold updated list of tasks
+    var updatedTaskArr = [];
+
+    // loop through current tasks
+    for (var i = 0; i < tasks.length; i++) {
+        // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
+        if (tasks[i].id !== parseInt(taskId)) {
+            updatedTaskArr.push(tasks[i]);
+        }
+    }
+
+    // reassign tasks array to be the same as updatedTaskArr
+    tasks = updatedTaskArr;
 };
 
-var editTask = function (taskId) {
+function editTask(taskId) {
     console.log("editing task #" + taskId);
 
     // get task list item element
@@ -167,7 +191,7 @@ var editTask = function (taskId) {
     document.querySelector("#save-task").textContent = "Save Task";
     formEl.setAttribute("data-task-id", taskId);
 
-};
+}
 
 var taskStatusChangeHandler = function (event) {
     // get the task item's id
@@ -188,6 +212,12 @@ var taskStatusChangeHandler = function (event) {
     else if (statusValue === "completed") {
         tasksCompletedEl.appendChild(taskSelected);
     }
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+            tasks[i].status = statusValue;
+        }
+    }
+    console.log(tasks);
 };
 pageContentEl.addEventListener("click", taskButtonHandler)
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
